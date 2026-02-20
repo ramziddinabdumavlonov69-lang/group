941c88ead0a903e0da6338c18bd9d720884d7cd6
salohiddinbtrv-star <salohiddinbtrv@gmail.com>
2026-02-20 07:22:23 +0300

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,1 +1,14 @@
 
+import os
+import shutil
+
+FILES_DIR = "files"
+
+if not os.path.exists(FILES_DIR):
+    os.makedirs(FILES_DIR)
+
+def save_file(filepath):
+    filename = os.path.basename(filepath)
+    destination = os.path.join(FILES_DIR, filename)
+    shutil.copy(filepath, destination)
+    return filename
